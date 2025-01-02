3e696f9d96bcf3b28a736f97c769c62b7f732581
sammytsukino <samy.cabello.g@gmail.com>
2025-01-02 12:34:48 +0100

    Gui updated

--- sketch-08.js
+++ sketch-08.js
@@ -1,6 +1,7 @@
 const canvasSketch = require("canvas-sketch");
 const random = require("canvas-sketch-util/random");
 const math = require("canvas-sketch-util/math");
+const Tweakpane = require("tweakpane");
 
 const settings = {
   dimensions: [1080, 1080],
