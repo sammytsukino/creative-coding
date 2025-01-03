4de22e656c8c5bc23a9d1c57d7bd3bd6904506b0
sammytsukino <samy.cabello.g@gmail.com>
2025-01-03 13:56:22 +0100

    New type sketch added

--- sketch-11.js
+++ sketch-11.js
@@ -1,14 +1,54 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [1080, 1080],
 };
+
+let manager;
+
+let text = "S"; 
+let fontSize = 800;
+let fontFamily = '"Arial Black"'; 
 
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
+    context.fillStyle = 'black';
+    context.font = `${fontSize}px ${fontFamily}`;
+    context.textBaseline = 'top';
+
+    const metrics = context.measureText(text);
+    const mx = metrics.actualBoundingBoxLeft * -1;
+    const my = metrics.actualBoundingBoxAscent * -1;
+    const mw = metrics.actualBoundingBoxRight + metrics.actualBoundingBoxLeft;
+    const mh = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+
+    const x = (width - mw) * 0.5 - mx;
+    const y = (height - mh) * 0.5 - my;
+
+    context.save();
+    context.translate(x, y);
+
+    context.beginPath();
+    context.rect(mx, my, mw, mh);
+    context.stroke();
+
+    context.fillText(text, 0, 0);
+    context.restore();
   };
 };
 
-canvasSketch(sketch, settings);
+const onKeyUp = (e) => {
+  text = e.key.toUpperCase();
+  manager.render();
+};
+
+document.addEventListener("keyup", onKeyUp);
+
+const start = async () => {
+  manager = await canvasSketch(sketch, settings);
+};
+
+start();
